eaeda80e3284961528cb419f7cfeb0eff1d2ccb2
panachainy <panachai.ny@gmail.com>
2020-03-08 18:04:32 +0700

    clean

--- src/main/java/com/example/demo/controller/HealthController.java
+++ src/main/java/com/example/demo/controller/HealthController.java
@@ -1,11 +1,8 @@
 package com.example.demo.controller;
 
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 public class HealthController {
